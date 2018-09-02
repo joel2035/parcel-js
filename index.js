9042d57c84d9f71dbfef769b75a51f3de23049b1
joel2035 <joel.pokam@hetic.net>
2018-09-02 13:26:17 +0200

    hommeAnimation

--- index.js
+++ index.js
@@ -4,6 +4,33 @@ import 'flexboxgrid';
 
 import jquery from 'jquery'
 window.$ = window.jQuery = jquery
-$('.yaya').animate({
-  fontSize: "50px",
-}, 2000)
+$(".features_home").animate({
+  top: "0%",
+  height: "100%",
+}, 500, function() {
+  $(".features_home").animate({
+    left: "0px",
+    width: "50%",
+
+  }, 900, function() {
+    $(".features_text").fadeIn(2000, function() {
+      $(".features_text").hide(900)
+      $(".features_home").animate({
+
+        width: "35%",
+      }, 500, function() {
+        $(".features_img").animate({
+            height: "70%",
+          }, 500,
+          function() {
+            $(".features_img").animate({
+              width: "30%",
+            }, 500)
+          })
+      })
+
+    })
+
+  })
+
+})
